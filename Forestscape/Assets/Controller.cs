30ba7dec126c36f7b3a96b47488df0970c535933
Mymi18 <2080611@bdeb.qc.ca>
2022-03-19 17:09:51 -0400

    try to import an book

--- Forestscape/Assets/Controller.cs
+++ Forestscape/Assets/Controller.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Controller : MonoBehaviour
 {
-
-    public Text message;
+    //[Tooltip()]
+    [SerializeField] private Text message;
 
     // Start is called before the first frame update
     void Start()
@@ -25,5 +26,6 @@ public class Controller : MonoBehaviour
         Debug.Log("go======");
 
         message.text = "Message";
+        SceneManager.LoadScene("");
     }
 }
