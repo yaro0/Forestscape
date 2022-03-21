d2e4aa71dee14ee79645734b367dbd8aab538e64
Mymi18 <2080611@bdeb.qc.ca>
2022-03-21 14:17:41 -0400

    make it happen

--- Forestscape/Assets/Controller.cs
+++ Forestscape/Assets/Controller.cs
@@ -2,12 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 
 public class Controller : MonoBehaviour
 {
-    //[Tooltip()]
-    [SerializeField] private Text message;
+
+    public Text message;
 
     // Start is called before the first frame update
     void Start()
@@ -26,6 +25,5 @@ public class Controller : MonoBehaviour
         Debug.Log("go======");
 
         message.text = "Message";
-        SceneManager.LoadScene("");
     }
 }
