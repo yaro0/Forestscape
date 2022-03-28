76e99a65473e68a766c6585905bea8feb87d4f4e
yaro0 <93663497+yaro0@users.noreply.github.com>
2022-03-28 15:18:13 -0400

    Time of day update
    Player goes back to base at night
    Clock works

--- Forestscape/Assets/script.cs
+++ Forestscape/Assets/script.cs
@@ -1,21 +1,41 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class script : MonoBehaviour
 {
+    public GameObject player;
+    public GameObject sceneManager;
+    private LightingManager lightingManager;
 
     GameObject x;
+    public Transform Target;
 
+    private bool playerIsAtBase;
     // Start is called before the first frame update
     void Start()
     {
-      
+
+        playerIsAtBase = false;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        returnPlayerToBase();
+    }
+
+    private void returnPlayerToBase()
+    {
+        float time = sceneManager.GetComponent<LightingManager>().getTimeOfDay();
+
+        if (time >= 2 && time <= 6 && !playerIsAtBase)
+        {
+            player.transform.position = Target.position;
+            //player.GetComponent<SUPERCharacterAIO>().enableMovementControl();
+        }
     }
 }
+
+
