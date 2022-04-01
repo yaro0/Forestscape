fbc4c4ddd5b8713cade19f8b44f254d24a1a3dbd
Mymi18 <2080611@bdeb.qc.ca>
2022-04-01 11:51:04 -0400

    Revert "Try to make it work to switch scenes"
    This reverts commit 0f79546dd26504e42fe2baad649b45c66d3c8fb6.

--- Forestscape/Assets/Scripts/SceneSwitcher.cs
+++ Forestscape/Assets/Scripts/SceneSwitcher.cs
@@ -1,18 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class SceneSwitcher : MonoBehaviour
 {
-    public void GoToWorkbench(/*string sceneName*/)
+    public void LoadScene(string sceneName)
     {
-        SceneManager.LoadScene(/*sceneName*/SceneManager.GetActiveScene().buildIndex + 1);
-    }
-
-    public void GoToMenu()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        SceneManager.LoadScene(sceneName);
     }
 }
