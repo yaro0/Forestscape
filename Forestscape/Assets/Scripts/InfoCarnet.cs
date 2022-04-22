9bc3fabe5e9a2282b454d4885fdf9624375b7a87
Mymi18 <2080611@bdeb.qc.ca>
2022-04-22 16:24:19 -0400

    making carnet

--- Forestscape/Assets/Scripts/InfoCarnet.cs
+++ Forestscape/Assets/Scripts/InfoCarnet.cs
@@ -2,17 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName ="New Item", menuName ="Data/Items")]
 public class InfoCarnet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Sprite photo;
+    public string descriptionText;
 }
