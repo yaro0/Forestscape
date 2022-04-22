673fcb83dfe80003af94f6a8c3ab37cc9a18e6aa
Mymi18 <2080611@bdeb.qc.ca>
2022-04-22 16:36:18 -0400

    make an asset for carnet

--- Forestscape/Assets/Scripts/InfoCarnet.cs
+++ Forestscape/Assets/Scripts/InfoCarnet.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New Item", menuName = "Data/Items", order = 8)]
-public class InfoCarnet : MonoBehaviour
+public class InfoCarnet : ScriptableObject
 {
     public Sprite photo;
     public string descriptionText;
