ee84b75064e9e933db1fc7cfdae732c09bdba938
Mymi18 <2080611@bdeb.qc.ca>
2022-04-23 19:56:36 -0400

    make carnet

--- Forestscape/Assets/Scripts/InfoCarnet.cs
+++ Forestscape/Assets/Scripts/InfoCarnet.cs
@@ -2,14 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-<<<<<<< HEAD
-[CreateAssetMenu(fileName = "New Item", menuName = "Data/Items")]
-public class InfoCarnet : ScriptableObject
-=======
-[CreateAssetMenu(fileName = "New Item", menuName = "Data/Items", order = 8)]
+
+
 public class InfoCarnet : MonoBehaviour
->>>>>>> parent of 673fcb83 (make an asset for carnet)
 {
-    public Sprite photo;
-    public string descriptionText;
+    
 }
