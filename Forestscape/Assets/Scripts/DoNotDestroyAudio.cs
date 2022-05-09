d471d25fbbaa1f9a7eb8bf2be128e79af61795c6
Alina-Alen <alina.alen.2003@gmail.com>
2022-05-09 02:06:52 -0700

    Added intro video

--- Forestscape/Assets/Scripts/DoNotDestroyAudio.cs
+++ Forestscape/Assets/Scripts/DoNotDestroyAudio.cs
@@ -2,17 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class DontDestroyAudio : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        DontDestroyOnLoad(transform.gameObject);
     }
 }
---
+++ Forestscape/Assets/Scripts/VideoTransition.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class VideoTransition : MonoBehaviour
+{
+
+    public VideoPlayer vid;
+    public GameObject videoViewer;
+
+    void Start()
+    {
+       
+        vid.loopPointReached += CheckOver;
+        videoViewer.SetActive(true);
+        vid.enabled = true;
+        vid.Play();
+    }
+
+
+    void CheckOver(UnityEngine.Video.VideoPlayer vp)
+    {
+        print("Video Is Over");
+        videoViewer.SetActive(false);
+        vid.enabled = false;
+     
+    }
+
+}
+
